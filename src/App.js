944e3f224de0b8cefe9f2d9dd54995ffafc262a3
mubin galib <amubin19@gmail.com>
2019-12-05 20:05:36 +0600

    Display static data.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
+function Todo ({todo, index}){
+  return <div className="todo">{todo.text}</div>
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([
+      {
+        text: 'Text One',
+        isCompleted: false
+      },
+      {
+        text: 'Text Two',
+        isCompleted: false
+      },
+      {
+        text: 'Text Three',
+        isCompleted: false
+      },
+      {
+        text: 'Text Four',
+        isCompleted: false
+      }
+
+    ]);
+
+    return (
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) =>(
+            <Todo key={index} index={index} todo={todo} />
+          ) 
+            
+          )}
+        </div>
+      </div>
+    )
 }
 
 export default App;
